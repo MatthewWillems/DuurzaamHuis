73bd8723b6e7c57701cb2902ebcfa1ef91e4cd25
Rafboehout <70891398+RafBoerhout@users.noreply.github.com>
2021-03-29 01:28:19 +0200

    Code sprint 2 af !

--- script.js
+++ script.js
@@ -2,6 +2,36 @@ var jcontent = {
     
 }
 
-var output = document.getElementById('output')
+var output = document.getElementById('licht')
 
 output.innerHTML = '12KwH';
+
+
+
+var jcontent = {
+    
+}
+
+var output = document.getElementById('app')
+
+output.innerHTML = '36Kwh';
+
+
+
+var jcontent = {
+    
+}
+
+var output = document.getElementById('temp')
+
+output.innerHTML = '15°C';
+
+
+
+var jcontent = {
+    
+}
+
+var output = document.getElementById('besp')
+
+output.innerHTML = '23€';
