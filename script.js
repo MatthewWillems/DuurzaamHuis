f37a3ff13be7e798b8c19c7bb22499c9ce8583f2
Matthew Willems <matthewwillems2003@outlook.com>
2021-04-12 11:35:56 +0200

    versie zonder iframe

---
+++ script.js
@@ -0,0 +1,37 @@
+var jcontent = {
+    
+}
+
+var output = document.getElementById('licht')
+
+output.innerHTML = '12KwH';
+
+
+
+var jcontent = {
+    
+}
+
+var output = document.getElementById('app')
+
+output.innerHTML = '36Kwh';
+
+
+
+var jcontent = {
+    
+}
+
+var output = document.getElementById('temp')
+
+output.innerHTML = '15°C';
+
+
+
+var jcontent = {
+    
+}
+
+var output = document.getElementById('besp')
+
+output.innerHTML = '23€';
